ee1c30fe722db985480a7c141ee272833fae1b90
Avoler0 <91608021+Avoler0@users.noreply.github.com>
2021-10-15 00:27:55 +0900

    v1 commit

---
+++ colors.js
@@ -0,0 +1,33 @@
+var Links ={
+  setColor:function(color){
+    var alist = document.querySelectorAll('a');
+    var i=0;
+    while(1 < alist.length){
+      alist[i].style.color=color;
+      i = i + 1;
+    }
+  }
+}
+var Body ={
+  setColor:function(color){
+    document.querySelector('body').style.color = color;
+  },
+  setBackgroundColor:function(color){
+    document.querySelector('body').style.backgroundColor = color;
+  }
+}
+function lightDarkHandler(self){
+    if(self.value ==='Dark'){
+      Body.setBackgroundColor('#151313');
+      Body.setColor('white');
+      self.value = 'Light';
+
+      Links.setColor('powderblue');
+    }else{
+      Body.setBackgroundColor('white');
+      Body.setColor('black');
+      self.value = 'Dark';
+
+      Links.setColor('blue');
+}
+}
